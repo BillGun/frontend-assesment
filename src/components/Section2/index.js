a469c1a2ff6be7d5edae689c891a3227fe700e51
BillGun <privatebillgun@gmail.com>
2019-09-19 10:39:49 +0700

    Doing some section 2

--- src/components/Section2/index.js
+++ src/components/Section2/index.js
@@ -1,4 +1,5 @@
 import React, { Component, Fragment } from "react";
+import UserSVG from "../../../public/assets/illustration-fr.svg";
 
 class Section2 extends Component {
   render() {
@@ -6,10 +7,45 @@ class Section2 extends Component {
       <section className="section-2">
         <div className="col-md-12">
           <div className="row">
-            <div className="offset-md-3 col-md-9">
+            <div className="offset-md-3 col-md-6">
               <h2 className="title">
                 <span className="underline"> What </span>We Do
               </h2>
+              <div className="section-2-content">
+                <div className="row">
+                  <div className="col-md-4">
+                    <div className="content-number">
+                      <p>1</p>
+                    </div>
+                    <div className="content-image">
+                      <img src={UserSVG}></img>
+                    </div>
+                  </div>
+                  <div className="col-md-8">
+                    <div className="content-description card-reverse">
+                      <div className="content-description-title">
+                        <p>
+                          <span className="card-glow">Face</span> Recognition
+                        </p>
+                        <hr></hr>
+                      </div>
+                      <div className="content-description-body">
+                        <p>
+                          Cras quis nulla commodo, aliquam lectus sed, blandit
+                          augue. Cras ullamcorper bibendum bibendum. Duis
+                          tincidunt urna non pretium porta. Nam condimentum
+                          vitae ligula vel ornare. Phasellus at semper turpis.
+                          Cras quis nulla commodo, aliquam lectus sed, blandit
+                          augue. Cras ullamcorper bibendum bibendum.
+                        </p>
+                      </div>
+                      <div className="content-description-footer">
+                        <p>Learn More -></p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         </div>
